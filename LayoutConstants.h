b9ab707bb803170c01f96f5eccfff71416c26c60
Christof Ruch <christof.ruch@gmx.de>
2021-06-03 23:48:29 +0200

    Starting a file with layout constants to make the layouts less adhoc

---
+++ LayoutConstants.h
@@ -0,0 +1,21 @@
+/*
+   Copyright (c) 2021 Christof Ruch. All rights reserved.
+
+   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+*/
+
+#pragma once
+
+#define LAYOUT_INSET_SMALL 4
+#define LAYOUT_INSET_NORMAL 8
+
+#define LAYOUT_LINE_HEIGHT 24
+#define LAYOUT_LINE_SPACING 32
+
+#define LAYOUT_LARGE_LINE_HEIGHT 36
+
+#define LAYOUT_CHECKBOX_WIDTH 100
+
+#define LAYOUT_LARGE_FONT_SIZE 24.0f
+
+
