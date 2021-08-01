108faea9b1ed4168123e47d5fb69ce136724fe20
Christof Ruch <christof.ruch@gmx.de>
2021-08-01 17:01:59 +0200

    Refactored - moving the GroupNode from the PatchListTree into a generic class TreeViewNode, as there is no KnobKraft specific code in the class.

---
+++ TreeViewNode.cpp
@@ -0,0 +1,125 @@
+/*
+   Copyright (c) 2021 Christof Ruch. All rights reserved.
+
+   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+*/
+
+#include "TreeViewNode.h"
+
+
+
+TreeViewNode::TreeViewNode(String text, String id) : text_(text), id_(id)
+{
+
+}
+
+void TreeViewNode::toggleOpenness()
+{
+	auto open = getOpenness();
+	switch (open) {
+	case Openness::opennessClosed:
+		setOpenness(TreeViewItem::opennessOpen);
+		break;
+	case Openness::opennessOpen:
+		setOpenness(TreeViewItem::opennessClosed);
+		break;
+	case Openness::opennessDefault:
+		// This only works as expected when the TreeView has default openness = closed
+		setOpenness(TreeViewItem::opennessOpen);
+		break;
+	}
+}
+
+bool TreeViewNode::mightContainSubItems()
+{
+	return onGenerateChildren != nullptr;
+}
+
+void TreeViewNode::itemOpennessChanged(bool isNowOpen)
+{
+	if (onGenerateChildren != nullptr && isNowOpen) {
+		regenerate();
+	}
+}
+
+void TreeViewNode::paintItem(Graphics& g, int width, int height)
+{
+	auto& lf = LookAndFeel::getDefaultLookAndFeel();
+	g.setColour(lf.findColour(Label::textColourId));
+	g.drawText(text_, 0, 0, width, height, Justification::centredLeft);
+}
+
+void TreeViewNode::paintOpenCloseButton(Graphics& g, const Rectangle<float>& area, Colour backgroundColour, bool isMouseOver)
+{
+	ignoreUnused(backgroundColour);
+	TreeViewItem::paintOpenCloseButton(g, area, LookAndFeel::getDefaultLookAndFeel().findColour(TreeView::backgroundColourId), isMouseOver);
+}
+
+bool TreeViewNode::canBeSelected() const
+{
+	return onSelected != nullptr;
+}
+
+void TreeViewNode::itemSelectionChanged(bool isNowSelected)
+{
+	if (isNowSelected) {
+		onSelected(id_);
+	}
+}
+
+void TreeViewNode::regenerate()
+{
+	if (onGenerateChildren) {
+		clearSubItems();
+		auto children = onGenerateChildren();
+		for (auto c : children) {
+			addSubItem(c);
+		}
+	}
+}
+
+juce::String TreeViewNode::getUniqueName() const
+{
+	if (id_.isNotEmpty()) {
+		return id_;
+	}
+	else {
+		return text_;
+	}
+}
+
+void TreeViewNode::itemClicked(const MouseEvent&)
+{
+	if (onSingleClick) {
+		onSingleClick(id_);
+	}
+}
+
+void TreeViewNode::itemDoubleClicked(const MouseEvent&)
+{
+	if (onDoubleClick) {
+		onDoubleClick(id_);
+	}
+}
+
+bool TreeViewNode::isInterestedInDragSource(const DragAndDropTarget::SourceDetails&)
+{
+	return onItemDropped != nullptr;
+}
+
+void TreeViewNode::itemDropped(const DragAndDropTarget::SourceDetails& dragSourceDetails, int insertIndex)
+{
+	String name = dragSourceDetails.description;
+	SimpleLogger::instance()->postMessage("Item dropped: " + name + " at " + String(insertIndex));
+	onItemDropped(dragSourceDetails.description);
+}
+
+juce::String TreeViewNode::id() const
+{
+	return id_;
+}
+
+juce::String TreeViewNode::text() const
+{
+	return text_;
+}
---
+++ TreeViewNode.h
@@ -0,0 +1,51 @@
+/*
+   Copyright (c) 2021 Christof Ruch. All rights reserved.
+
+   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+*/
+
+#include "JuceHeader.h"
+
+#include "Logger.h"
+
+class TreeViewNode : public TreeViewItem {
+public:
+	typedef std::function<std::vector<TreeViewItem*>()> TChildGenerator;
+	typedef std::function<void(String)>  TClickedHandler;
+	typedef std::function<void(juce::var)>  TDropHandler;
+
+	TreeViewNode(String text, String id);
+
+	TChildGenerator onGenerateChildren;
+	TClickedHandler onSelected;
+	TClickedHandler onSingleClick;
+	TClickedHandler onDoubleClick;
+	TDropHandler    onItemDropped;
+
+	String id() const;
+	String text() const;
+
+	void toggleOpenness();
+	void regenerate();
+
+	virtual bool mightContainSubItems() override;
+	virtual void itemOpennessChanged(bool isNowOpen) override;
+	virtual void paintItem(Graphics& g, int width, int height) override;
+	virtual void paintOpenCloseButton(Graphics& g, const Rectangle<float>& area, Colour backgroundColour, bool isMouseOver) override;
+	virtual bool canBeSelected() const override;
+	virtual void itemSelectionChanged(bool isNowSelected) override;
+
+	virtual String getUniqueName() const override;
+	virtual void itemClicked(const MouseEvent&) override;
+	virtual void itemDoubleClicked(const MouseEvent&) override;
+	virtual bool isInterestedInDragSource(const DragAndDropTarget::SourceDetails&) override;
+	virtual void itemDropped(const DragAndDropTarget::SourceDetails& dragSourceDetails, int insertIndex) override;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(TreeViewNode)
+
+private:
+	String text_;
+	String id_;
+};
+
+
